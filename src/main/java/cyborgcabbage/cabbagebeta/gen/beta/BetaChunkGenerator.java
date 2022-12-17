2f72d2aae0bb2e7312238c34fcb0320a163ef941
FinlaySML <2100719@uad.ac.uk>
2022-12-17 19:33:52 +0000

    Version 1.1.1

--- src/main/java/cyborgcabbage/cabbagebeta/gen/beta/BetaChunkGenerator.java
+++ src/main/java/cyborgcabbage/cabbagebeta/gen/beta/BetaChunkGenerator.java
@@ -171,6 +171,7 @@ public class BetaChunkGenerator extends ChunkGenerator {
     @Override
     public void generateFeatures(StructureWorldAccess world, Chunk chunk, StructureAccessor structureAccessor) {
         //super.generateFeatures(world, chunk, structureAccessor);
+        generator.init(world.getSeed());
         generator.populate(world, chunk);
 
     }
@@ -204,6 +205,7 @@ public class BetaChunkGenerator extends ChunkGenerator {
 
     @Override
     public CompletableFuture<Chunk> populateNoise(Executor executor, Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
+        generator.init(noiseConfig.getLegacyWorldSeed());
         generator.fillChunk(chunk);
         return CompletableFuture.completedFuture(chunk);
     }
